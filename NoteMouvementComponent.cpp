0980729530a52a30ec71cc517e884811c459e0f1
Lucas Bruel <81522727+Lurius-Kitsune@users.noreply.github.com>
2025-01-28 14:39:21 +0100

    Music notes now fall

--- NoteMouvementComponent.cpp
+++ NoteMouvementComponent.cpp
@@ -7,6 +7,12 @@ NoteMouvementComponent::NoteMouvementComponent(Actor* _owner, float _speed)
 	speed = _speed;
 }
 
+NoteMouvementComponent::NoteMouvementComponent(Actor* _owner, const NoteMouvementComponent& _other)
+	: Component(_owner)
+{
+	speed = _other.speed;
+}
+
 void NoteMouvementComponent::Tick(const float _deltaTime)
 {
 	Move(_deltaTime);
@@ -14,5 +20,5 @@ void NoteMouvementComponent::Tick(const float _deltaTime)
 
 void NoteMouvementComponent::Move(const float _deltaTime)
 {
-	owner->Move(Vector2f(0, -1.0f) * speed * _deltaTime);
+	owner->Move(Vector2f(0, 1.0f) * speed * _deltaTime * 500.0f);
 }
