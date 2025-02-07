baa29cfede8a8249d4cec8fd1f5f3f88eb2d36a3
konishiba <yoan.gimenez1202@gmail.com>
2025-02-07 13:20:52 +0100

    ecf<

--- SelectLevel.cpp
+++ SelectLevel.cpp
@@ -1,12 +1,10 @@
 #include "SelectLevel.h"
 #include "CameraManager.h"
 #include "TimerManager.h"
-#include "GameManager.h"
 #include "FileManager.h"
 #include "HUD.h"
 #include "MeshActor.h"
 #include "ActorManager.h"
-#include "Image.h"
 #include "InputManager.h"
 #include "AudioManager.h"
 
@@ -14,7 +12,7 @@ using namespace Camera;
 using namespace UI;
 using namespace File;
 
-SelectLevel::SelectLevel()
+SelectLevel::SelectLevel() : Level("level") //Todo trouver un nom
 {
 	allTracks = {};
 	background = nullptr;
@@ -32,28 +30,28 @@ SelectLevel::~SelectLevel()
 
 void SelectLevel::InitSeparator()
 {
-	RectangleActor* _topSeparation = Level::SpawnActor(RectangleActor(RectangleShapeData(Vector2f(windowSize.x, 5.0f), "background"))); //TODO implemant Font
-	_topSeparation->SetFillColor(Color(255, 255, 255, 150));
+	RectangleActor* _topSeparation = Level::SpawnActor<RectangleActor>(RectangleActor(RectangleShapeData(Vector2f(windowSize.x, 5.0f), "background"))); //TODO implemant Font
+	//_topSeparation->SetFillColor(Color(255, 255, 255, 150));
 	_topSeparation->SetPosition(Vector2f(0.0f, 60.0f));
 
-	RectangleActor* _levelSeparation = Level::SpawnActor(RectangleActor(RectangleShapeData(Vector2f(5.0f, windowSize.y - 120.0f), "background"))); //TODO implemant Font
-	_levelSeparation->SetFillColor(Color(255, 255, 255, 150));
+	RectangleActor* _levelSeparation = Level::SpawnActor<RectangleActor>(RectangleActor(RectangleShapeData(Vector2f(5.0f, windowSize.y - 120.0f), "background"))); //TODO implemant Font
+	//_levelSeparation->SetFillColor(Color(255, 255, 255, 150));
 	_levelSeparation->SetPosition(Vector2f(windowSize.x * 0.6f, _topSeparation->GetPosition().y));
 
-	RectangleActor* _bottomSeparation = Level::SpawnActor(RectangleActor(RectangleShapeData(Vector2f(windowSize.x, 5.0f), "background"))); //TODO implemant Font
-	_bottomSeparation->SetFillColor(Color(255, 255, 255, 150));
+	RectangleActor* _bottomSeparation = Level::SpawnActor<RectangleActor>(RectangleActor(RectangleShapeData(Vector2f(windowSize.x, 5.0f), "background"))); //TODO implemant Font
+	//_bottomSeparation->SetFillColor(Color(255, 255, 255, 150));
 	_bottomSeparation->SetPosition(Vector2f(0.0f, windowSize.y - 60.0f));
 }
 
 void SelectLevel::InitLabel()
 {
-	Label* _label = M_HUD.CreateWidget<Label>("Select Level", Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _label = M_HUD.CreateWidget<LabelWidget>("Select Level", "SelectLevelLabel"); //TODO implemant Font
 	_label->SetCharacterSize(35);
 	_label->SetZOrder(2);
 	_label->SetPosition(Vector2f(windowSize.x * 0.05f, 10.0f));
 
 	//TODO Replace with button
-	Label* _play = M_HUD.CreateWidget<Label>("PLAY", Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _play = M_HUD.CreateWidget<LabelWidget>("PLAY", "PlayLabel"); //TODO implemant Font
 	_play->SetCharacterSize(30);
 	_play->SetZOrder(2);
 	_play->SetPosition(Vector2f(windowSize.x * 0.875f, windowSize.y - 45.0f));
@@ -64,37 +62,37 @@ void SelectLevel::InitLabel()
 
 void SelectLevel::InitDescription()
 {
-	description = M_HUD.CreateWidget<Canvas>("Description", Screen);
-	Label* _title = M_HUD.CreateWidget<Label>("TrackTitle", Screen, "Test", TTF); //TODO implemant Font
+	description = M_HUD.CreateWidget<CanvasWidget>("Description", Screen);
+	LabelWidget* _title = M_HUD.CreateWidget<LabelWidget>("TrackTitle","TrackTitleLabel"); //TODO implemant Font
 	_title->SetCharacterSize(50);
 	_title->SetZOrder(2);
 	_title->SetPosition(Vector2f(windowSize.x * 0.65f, windowSize.y * 0.2f));
 
-	Label* _artist = M_HUD.CreateWidget<Label>("TrackTitle", Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _artist = M_HUD.CreateWidget<LabelWidget>("TrackTitle","TrackTitleLabel2"); //TODO implemant Font
 	_artist->SetCharacterSize(25);
 	_artist->SetZOrder(2);
 	_artist->SetPosition(Vector2f(windowSize.x * 0.65f, windowSize.y * 0.3f));
 
-	Label* _duration = M_HUD.CreateWidget<Label>("TrackDuration", Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _duration = M_HUD.CreateWidget<LabelWidget>("TrackDuration", "TrackDurationLabel"); //TODO implemant Font
 	_duration->SetCharacterSize(20);
 	_duration->SetZOrder(2);
 	_duration->SetPosition(Vector2f(windowSize.x * 0.8f, windowSize.y * 0.4f));
 
 	//TODO Dificulty
-	Label* _easy = M_HUD.CreateWidget<Label>("TrackDifficulty", Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _easy = M_HUD.CreateWidget<LabelWidget>("TrackDifficulty", "TrackDifficultyLabel"); //TODO implemant Font
 	_easy->SetCharacterSize(20);
 	_easy->SetZOrder(2);
 	_easy->SetFillColor(Color(50, 189, 22));
 	_easy->SetPosition(Vector2f(windowSize.x * 0.65, windowSize.y * 0.5f));
 
 
-	description->AddWidget(_title);
+	description->AddChild(_title);
 	infoLabel.insert(make_pair(TI_TITLE, _title));
-	description->AddWidget(_artist);
+	description->AddChild(_artist);
 	infoLabel.insert(make_pair(TI_ARTIST, _artist));
-	description->AddWidget(_duration);
+	description->AddChild(_duration);
 	infoLabel.insert(make_pair(TI_DURATION, _duration));
-	description->AddWidget(_easy);
+	description->AddChild(_easy);
 
 	M_HUD.AddToViewport(description);
 }
@@ -103,32 +101,33 @@ void SelectLevel::InitRectangleTrackInfo(Track* _track)
 {
 	if (allTracksCanvas.contains(_track)) return;
 
-	Canvas* _trackInfo = M_HUD.CreateWidget<Canvas>("TrackSelection", Screen);
-	UI::Image* _background = M_HUD.CreateWidget<UI::Image>("background", RectangleShapeData(Vector2f(windowSize.x * 0.58f, 50.0f), "background"), World); //TODO implemant Font
+	CanvasWidget* _trackInfo = M_HUD.CreateWidget<CanvasWidget>("TrackSelection", Screen);
+	UI::ImageWidget* _background = M_HUD.CreateWidget<UI::ImageWidget>("background", RectangleShapeData(Vector2f(windowSize.x * 0.58f, 50.0f), "background"), World); //TODO implemant Font
 	const Vector2f& _backgroundSize = _background->GetSize();
 	_background->SetZOrder(0);
 
-	Label* _title = M_HUD.CreateWidget<Label>(_track->GetTitle(), Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _title = M_HUD.CreateWidget<LabelWidget>(_track->GetTitle(),"TitleLabel"); //TODO implemant Font
 	_title->SetCharacterSize(20);
 	_title->SetZOrder(2);
 	_title->SetPosition(Vector2f(_backgroundSize.x * 0.01f, 0.0f));
 
-	Label* _artist = M_HUD.CreateWidget<Label>(_track->GetArtist(), Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _artist = M_HUD.CreateWidget<LabelWidget>(_track->GetArtist(),"ArtistLabel"); //TODO implemant Font
 	_artist->SetCharacterSize(20);
 	_artist->SetZOrder(2);
 	_artist->SetPosition(Vector2f(_backgroundSize.x * 0.01f, _backgroundSize.y * 0.5f));
 
-	Label* _duration = M_HUD.CreateWidget<Label>(_track->GetDurationAsString(), Screen, "Test", TTF); //TODO implemant Font
+	LabelWidget* _duration = M_HUD.CreateWidget<LabelWidget>(_track->GetDurationAsString(), "DurationLabel"); //TODO implemant Font
 	_duration->SetCharacterSize(20);
 	_duration->SetZOrder(2);
 	_duration->SetPosition(Vector2f(_backgroundSize.x * 0.5f, _backgroundSize.y * 0.25f));
 
-	_trackInfo->AddWidget(_background);
-	_trackInfo->AddWidget(_title);
-	_trackInfo->AddWidget(_artist);
-	_trackInfo->AddWidget(_duration);
+	_trackInfo->AddChild(_background);
+	_trackInfo->AddChild(_title);
+	_trackInfo->AddChild(_artist);
+	_trackInfo->AddChild(_duration);
 
-	_trackInfo->Hide();
+	_trackInfo->SetVisibility(VisibilityType::Hidden);
+	//_trackInfo->Hide();
 	allTracksCanvas.insert(make_pair(_track, _trackInfo));
 	
 	M_HUD.AddToViewport(_trackInfo);
@@ -136,14 +135,14 @@ void SelectLevel::InitRectangleTrackInfo(Track* _track)
 
 void SelectLevel::SetDescription(Track* _track)
 {
-	infoLabel[TI_TITLE]->SetString(_track->GetTitle());
-	infoLabel[TI_ARTIST]->SetString(_track->GetArtist());
-	infoLabel[TI_DURATION]->SetString(_track->GetDurationAsString());
+	infoLabel[TI_TITLE]->SetText(_track->GetTitle());
+	infoLabel[TI_ARTIST]->SetText(_track->GetArtist());
+	infoLabel[TI_DURATION]->SetText(_track->GetDurationAsString());
 }
 
 void SelectLevel::ChangeIterator(bool _isUp)
 {
-	M_AUDIO.Stop();
+	//M_AUDIO.Stop();
 	if (_isUp)
 	{
 		if (musicIterator == --allTracksCanvas.end())
@@ -168,15 +167,16 @@ void SelectLevel::ChangeIterator(bool _isUp)
 	}
 	(*musicIterator).first->PlayExtrait();
 	WheelCanvas();
+
 }
 
 void SelectLevel::SelectTrack()
 {
-	Canvas* _selectCanvas = (*musicIterator).second;
-	_selectCanvas->Show();
+	CanvasWidget* _selectCanvas = (*musicIterator).second;
+	_selectCanvas->SetVisibility(VisibilityType::Visible);
 	_selectCanvas->SetPosition(Vector2f(windowSize.x * 0.01f, 140.0f));
-	_selectCanvas->UpdateWidgets();
-	_selectCanvas->GetFirstWidgetOf<UI::Image>()->SetOutline(2.0f, Color(255, 255, 255));
+	//_selectCanvas->UpdateWidgets();
+	//_selectCanvas->GetFirstWidgetOf<UI::ImageWidget>()->SetOutline(2.0f, Color(255, 255, 255)); //TODO trouver solution
 }
 
 void SelectLevel::WheelCanvas()
@@ -200,11 +200,11 @@ void SelectLevel::WheelCanvas()
 			}
 			
 		}
-		Canvas* _currentCanvas = (*_current).second;
-		_currentCanvas->GetFirstWidgetOf<UI::Image>()->SetOutline(2.0f, Color(255, 255, 255, 0));
+		CanvasWidget* _currentCanvas = (*_current).second;
+		//_currentCanvas->GetFirstWidgetOf<UI::ImageWidget>()->SetOutline(2.0f, Color(255, 255, 255, 0));
 		_currentCanvas->SetPosition(Vector2f(windowSize.x * 0.01f, 70.0f * _index));
-		_currentCanvas->UpdateWidgets();
-		_currentCanvas->Show();
+		//_currentCanvas->UpdateWidgets();
+		_currentCanvas->SetVisibility(VisibilityType::Visible);
 	}
 	
 }
@@ -219,24 +219,32 @@ bool SelectLevel::CrampIterator(Iterator& _current)
 	return false;
 }
 
-void SelectLevel::Start()
+void SelectLevel::Load()
 {
-	Super::Start();
-	windowSize = CAST(Vector2f, M_GAME.GetCurrent()->GetWindowSize());
+}
 
-	M_CAMERA.CreateCamera<CameraActor>(FloatRect({ 0.0f, 0.0f }, windowSize), "DefaultCamera");
+void SelectLevel::Unload()
+{
+	Super::Unload();
+}
 
-	background = Level::SpawnActor(MeshActor(RectangleShapeData(windowSize, "background"))); //TODO implemant Font
+void SelectLevel::InitLevel()
+{
+	////windowSize = CAST(Vector2f, M_GAME.GetCurrent()->GetWindowSize());
+
+	////M_CAMERA.CreateCamera<CameraActor>(FloatRect({ 0.0f, 0.0f }, windowSize), "DefaultCamera");
+
+	background = Level::SpawnActor<MeshActor>(MeshActor(RectangleShapeData(windowSize, "background"))); //TODO implemant Font
 	background->SetOriginAtMiddle();
 	background->SetPosition(windowSize / 2.0f);
 	background->SetScale({ 1.2f, 2.0f });
 	background->SetRotation(degrees(45));
-	background->SetFillColor(Color(255, 255, 255, 100));
+	//background->SetFillColor(Color(255, 255, 255, 100));
 
 	allTracks = M_FILE.ReadFolder<Track>("Assets\\Tracks");
 	Track* _track = allTracks[trackIndex];
 
-	allButtons.push_back(new Button("Button", Screen, _track));
+	//allButtons.push_back(new ButtonWidget("Button", Screen, _track));
 
 	InitSeparator();
 	InitLabel();
@@ -250,29 +258,66 @@ void SelectLevel::Start()
 	musicIterator = allTracksCanvas.begin();
 
 
-	M_ACTOR.BeginPlay();
 	//M_INPUT.BindAction([&]() { ChangeIterator(true); }, Code::Z);
 	//M_INPUT.BindAction([&]() { ChangeIterator(false); }, Code::S);
 	(*musicIterator).first->PlayExtrait();
 	WheelCanvas();
 }
 
-bool SelectLevel::Update()
-{
-	Super::Update();
-	// Background
-	background->Rotate(degrees(M_TIMER.GetDeltaTime().asSeconds() * 10));
-	// Description
-	SetDescription((*musicIterator).first);
-	// Wheel
-	
+//void SelectLevel::Start()
+//{
+//	////Super::Start();
+//	////windowSize = CAST(Vector2f, M_GAME.GetCurrent()->GetWindowSize());
+//
+//	////M_CAMERA.CreateCamera<CameraActor>(FloatRect({ 0.0f, 0.0f }, windowSize), "DefaultCamera");
+//
+//	////background = Level::SpawnActor(MeshActor(RectangleShapeData(windowSize, "background"))); //TODO implemant Font
+//	//background->SetOriginAtMiddle();
+//	//background->SetPosition(windowSize / 2.0f);
+//	//background->SetScale({ 1.2f, 2.0f });
+//	//background->SetRotation(degrees(45));
+//	//background->SetFillColor(Color(255, 255, 255, 100));
+//
+//	//allTracks = M_FILE.ReadFolder<Track>("Assets\\Tracks");
+//	//Track* _track = allTracks[trackIndex];
+//
+//	//allButtons.push_back(new Button("Button", Screen, _track));
+//
+//	//InitSeparator();
+//	//InitLabel();
+//	//InitDescription();
+//
+//	//for (Track* _track : allTracks)
+//	//{
+//	//	InitRectangleTrackInfo(_track);
+//	//}
+//
+//	//musicIterator = allTracksCanvas.begin();
+//
+//
+//	//M_ACTOR.BeginPlay();
+//	////M_INPUT.BindAction([&]() { ChangeIterator(true); }, Code::Z);
+//	////M_INPUT.BindAction([&]() { ChangeIterator(false); }, Code::S);
+//	//(*musicIterator).first->PlayExtrait();
+//	//WheelCanvas();
+//}
 
-	return IsOver();
-}
+//bool SelectLevel::Update()
+//{
+//	Super::Update();
+//	// Background
+//	background->Rotate(degrees(M_TIMER.GetDeltaTime().asSeconds() * 10));
+//	// Description
+//	SetDescription((*musicIterator).first);
+//	// Wheel
+//	
+//
+//	return IsOver();
+//}
 
-void SelectLevel::Stop()
-{
-	Super::Stop();
-
-
-}
+//void SelectLevel::Stop()
+//{
+//	Super::Stop();
+//
+//
+//}
--- SelectLevel.h
+++ SelectLevel.h
@@ -1,58 +1,64 @@
-//#pragma once
-//#include "BeatMapLevel.h"
-//#include "MeshActor.h"
-//#include "Button.h"
-//#include "Canvas.h"
-//
-//enum TrackInfo
-//{
-//	TI_TITLE,
-//	TI_ARTIST,
-//	TI_DURATION,
-//	
-//	TI_COUNT,
-//};
-//
-//class SelectLevel : public Game
-//{
-//	using Iterator = unordered_map<Track*, Canvas*>::iterator;
-//
-//	MeshActor* background;
-//	Vector2f windowSize;
-//	vector<Button*> allButtons;
-//	vector<Track*> allTracks;
-//
-//	// Description
-//	unordered_map<Track*,Canvas*> allTracksCanvas;
-//	Iterator musicIterator; 
-//
-//
-//	// Right Info
-//	Canvas* description; 
-//	map<TrackInfo, Label*> infoLabel;
-//	u_int trackIndex;
-//
-//public:
-//	SelectLevel();
-//	virtual ~SelectLevel();
-//
-//private:
-//	// Init
-//	void InitSeparator();
-//	void InitLabel();
-//	void InitDescription();
-//	void InitRectangleTrackInfo(Track* _track);
-//
-//	// SetLabel
-//	void SetDescription(Track* _track);
-//	void ChangeIterator(bool _isUp);
-//
-//	void SelectTrack();
-//	void WheelCanvas();
-//
-//	bool CrampIterator(Iterator& _current);
-//public:
-//	virtual void Start() override;
-//	virtual bool Update() override;
-//	virtual void Stop() override;
-//};
+#pragma once
+#include "BeatMapLevel.h"
+#include "MeshActor.h"
+#include "ButtonWidget.h"
+#include "CanvasWidget.h"
+
+enum TrackInfo
+{
+	TI_TITLE,
+	TI_ARTIST,
+	TI_DURATION,
+	
+	TI_COUNT,
+};
+
+class SelectLevel : public Level
+{
+	using Iterator = unordered_map<Track*, CanvasWidget*>::iterator;
+
+	MeshActor* background;
+	Vector2f windowSize;
+	vector<ButtonWidget*> allButtons;
+	vector<Track*> allTracks;
+
+	// Description
+	unordered_map<Track*, CanvasWidget*> allTracksCanvas;
+	Iterator musicIterator; 
+
+
+	// Right Info
+	CanvasWidget* description;
+	map<TrackInfo, LabelWidget*> infoLabel;
+	u_int trackIndex;
+
+public:
+	SelectLevel();
+	virtual ~SelectLevel();
+
+private:
+	// Init
+	void InitSeparator();
+	void InitLabel();
+	void InitDescription();
+	void InitRectangleTrackInfo(Track* _track);
+
+	// SetLabel
+	void SetDescription(Track* _track);
+	void ChangeIterator(bool _isUp);
+
+	void SelectTrack();
+	void WheelCanvas();
+
+	bool CrampIterator(Iterator& _current);
+public:
+	virtual void Load() override;
+	virtual void Unload() override;
+
+protected:
+	virtual void InitLevel() override;
+
+	//virtual void Start() override;
+	//virtual bool Update() override;
+	//virtual void Stop() override;
+};
